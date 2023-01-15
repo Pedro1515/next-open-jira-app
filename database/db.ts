59aec51d8b772ba14e943ffd525600cceaa4939c
Pedro Moran <pedromoran228@gmail.com>
2023-01-15 17:38:06 -0300

    mongodb atlas connection

--- database/db.ts
+++ database/db.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 /**
  * 0 = disconnected
@@ -7,42 +7,45 @@ import mongoose from 'mongoose';
  * 3 = disconnecting
  */
 
+const MONGODB_URL =
+  "mongodb+srv://open_user:open_user_1234@clustertest.l6njp.mongodb.net/test";
+
 const mongooConnection = {
-    status: 0
-}
+  status: 0,
+};
 
 export const connect = async () => {
-    if (mongooConnection.status !== 0) {
-        console.log( 'we were already connected' )
-        return;
+  if (mongooConnection.status !== 0) {
+    console.log("we were already connected");
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongooConnection.status = mongoose.connections[0].readyState;
+
+    if (mongooConnection.status === 1) {
+      console.log("Using existing database connection");
+      return;
     }
 
-    if( mongoose.connections.length > 0 ) {
-        mongooConnection.status = mongoose.connections[0].readyState;
-        
-        if ( mongooConnection.status === 1 ) {
-            console.log( 'Using existing database connection' )
-            return;
-        }
+    await disconnect();
+  }
 
-        await disconnect()
-    }
+  mongooConnection.status = 2;
+  console.log("Connecting to database: ", MONGODB_URL);
 
-    mongooConnection.status = 2;
-    console.log('Connecting to database: ', process.env.MONGODB_URL)
-
-    await mongoose.connect( process.env.MONGODB_URL || '' )
-    mongooConnection.status = 1;
-    console.log('Connected to database: ', process.env.MONGODB_URL)
-}
+  await mongoose.connect(MONGODB_URL || "");
+  mongooConnection.status = 1;
+  console.log("Connected to database: ", MONGODB_URL);
+};
 
 export const disconnect = async () => {
-    if (mongooConnection.status === 0) return;
+  if (mongooConnection.status === 0) return;
 
-    mongooConnection.status = 3;
-    console.log('Disconnecting from database', process.env.MONGODB_URL)
-    
-    await mongoose.disconnect();
-    mongooConnection.status = 0;
-    console.log('Disconnected from database', process.env.MONGODB_URL)
-}
+  // mongooConnection.status = 3;
+  // console.log("Disconnecting from database", MONGODB_URL);
+
+  // await mongoose.disconnect();
+  // mongooConnection.status = 0;
+  // console.log("Disconnected from database", MONGODB_URL);
+};
